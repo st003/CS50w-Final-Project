46edebfab6e8cc0e73fd584ee0444445cb228e29
st003 <stuart.j.calcote@gmail.com>
2019-06-20 19:28:01 -0700

    improved main nav mobile behavior

--- purchasing/static/purchasing/scripts.js
+++ purchasing/static/purchasing/scripts.js
@@ -1,8 +1,25 @@
+const MEDIUMBREAKPOINT = 768;
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    // side bar nav active styling
-    if (document.querySelector('.sideBar')) {
+    // main nav scripts
+    if (document.querySelector('#mainNav')) {
 
+        // set mobile menu to be collaped onload 
+        if (window.innerWidth <= MEDIUMBREAKPOINT) {
+            document.querySelector('#navCollapse').classList.remove('show');
+        }
+
+        // close mobile menu or show deskop menu on window resize
+        window.addEventListener('resize', () => {
+            if (window.innerWidth > MEDIUMBREAKPOINT) {
+                document.querySelector('#navCollapse').classList.add('show');
+            } else if (window.innerWidth <= MEDIUMBREAKPOINT) {
+                document.querySelector('#navCollapse').classList.remove('show');
+            }
+        });
+
+        // side bar nav active styling
         const contentArea = document.querySelector('.page');
         document.querySelectorAll('.sideBarLink').forEach( (link) => {
             if (link.dataset.nav == contentArea.dataset.page)
